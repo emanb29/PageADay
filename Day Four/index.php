feb865fae66844f81163836be9c6466a84b71714
emanb29 <ebgamer29@gmail.com>
2013-07-28 15:05:33 -0700

    Day 4
    Still ahead, but whatever.

---
+++ Day Four/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Lowen Family Hub</title>
+  <link rel="stylesheet" type="text/css" href="css/fonts.css">
+  <link rel="stylesheet" type="text/css" href="css/reset.css">  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script type="text/javascript" src="js/jq.js"></script>
+  <script type="text/javascript" src="js/navhandler.js"></script>
+  <script type="text/javascript" src="js/lvlcolumns.js"></script>
+
+</head>
+<body id="day4">
+  <nav id="mainnav">
+    <div id="bar"></div>
+    <ul>
+      <li class="active"><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#donate">Donate</a></li>
+
+      <input id="search" type="text"></input><img id="searchicon" src="images/searchicon.png" />
+    </ul>
+    <hr>
+  </nav>
+
+  <header>
+    <img id="logo" src="images/NDLogo.png" />
+  </header>
+
+  <div id="main">
+    <div id="content" class="col">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis molestie posuere. Pellentesque ullamcorper ligula eget nibh adipiscing sit amet lacinia velit sagittis. Sed lobortis sollicitudin iaculis. Curabitur libero nulla, varius in imperdiet quis, volutpat in risus. Nam ac enim nec lectus consequat sodales non et neque. Fusce ornare magna ut magna pulvinar pulvinar. Quisque commodo mollis lorem, et mattis dolor commodo a. Curabitur vitae risus nunc, commodo semper tellus. Donec gravida, urna vitae ornare fermentum, dui orci venenatis orci, in vehicula neque ante non lorem. Aliquam in diam a erat porta bibendum. Pellentesque non nisl nec nunc ullamcorper mattis. Maecenas mauris sem, lobortis a fermentum quis, elementum non tellus. Integer pellentesque dui est, sed mollis nulla. Sed vitae velit nisl. Cras ligula est, placerat at sagittis et, hendrerit sed ante. Maecenas a dolor arcu.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Mauris dignissim fringilla mi a gravida. Pellentesque elementum, tellus et vestibulum egestas, lorem eros laoreet augue, at feugiat lorem purus eu justo. Integer adipiscing, risus a fringilla eleifend, lectus velit ultricies nisi, nec sagittis felis augue vel dolor. Phasellus eros metus, aliquet non faucibus id, condimentum eu diam. Aliquam erat volutpat. Mauris in neque quis dui congue euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mauris nulla, congue gravida rhoncus sed, molestie vitae enim.</p>
+      <p>Nulla sed blandit lorem. Praesent quis metus orci. Proin a tincidunt mi. Quisque porttitor, diam et vehicula suscipit, lacus nulla suscipit dolor, vitae pulvinar lacus nisl at magna. Maecenas ornare euismod sem vel eleifend. Sed rutrum, quam non luctus convallis, neque orci commodo libero, a pulvinar turpis lectus vitae tellus. Sed ut nisi vel tortor auctor imperdiet id ac erat. Duis sit amet dictum ligula. Suspendisse ac justo augue, id suscipit diam. Ut rhoncus eros eu ligula lobortis vel fringilla arcu sagittis. Sed luctus arcu sit amet nunc viverra consequat. Nulla sed ligula blandit elit auctor hendrerit. Pellentesque massa augue, volutpat eu porttitor eget, dignissim quis ipsum.</p>
+    </div>
+    <div id="sidebar" class="col">
+      <p>Aenean nibh ipsum, venenatis vitae sodales id, euismod eu libero. Suspendisse potenti. Cras sit amet urna eget nisl porttitor fringilla. Praesent eleifend tempor bibendum. Ut rutrum tempor tellus eget tristique. Nam fermentum placerat libero, in hendrerit nisl tempor ac. In vitae metus ut magna posuere sollicitudin ut a purus. Mauris viverra magna sit amet nisi facilisis laoreet nec eu elit. Suspendisse gravida quam et nisl eleifend posuere. Donec accumsan lectus vitae nulla commodo euismod. Ut sit amet lectus risus, eget porta mauris. Sed at lorem sed est ornare fermentum quis nec orci. Nunc tristique augue at mauris porta tempor.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Mauris dignissim fringilla mi a gravida. Pellentesque elementum, tellus et vestibulum egestas, lorem eros laoreet augue, at feugiat lorem purus eu justo. Integer adipiscing, risus a fringilla eleifend, lectus velit ultricies nisi, nec sagittis felis augue vel dolor. Phasellus eros metus, aliquet non faucibus id, condimentum eu diam. Aliquam erat volutpat. Mauris in neque quis dui congue euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mauris nulla, congue gravida rhoncus sed, molestie vitae enim.</p>
+      <p>Nulla sed blandit lorem. Praesent quis metus orci. Proin a tincidunt mi. Quisque porttitor, diam et vehicula suscipit, lacus nulla suscipit dolor, vitae pulvinar lacus nisl at magna. Maecenas ornare euismod sem vel eleifend. Sed rutrum, quam non luctus convallis, neque orci commodo libero, a pulvinar turpis lectus vitae tellus. Sed ut nisi vel tortor auctor imperdiet id ac erat. Duis sit amet dictum ligula. Suspendisse ac justo augue, id suscipit diam. Ut rhoncus eros eu ligula lobortis vel fringilla arcu sagittis. Sed luctus arcu sit amet nunc viverra consequat. Nulla sed ligula blandit elit auctor hendrerit. Pellentesque massa augue, volutpat eu porttitor eget, dignissim quis ipsum.</p>
+    </div>
+  </div>
+  
+  <footer>
+    <hr>
+    <div id="footertext">
+      <span class="themeblue">The author couldn't decide on sample footer content</span>
+      <br>
+      <span class="white">So, here I am</span>
+    </div>
+    <div id="bottombar"><div id="credits">Thanks to various contributors for the inspiration, icons, and fonts!</div></div>
+  </footer>
+</body>
+</html>
